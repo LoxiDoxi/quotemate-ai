c1dd5069ed8e1c84e692398b694585152f9c10b3
LoxiDoxi <Lockielad15@outlook.com>
2026-08-02 07:21:16 -0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -83,23 +83,19 @@ export default function RootLayout({
 
       <body className="min-h-screen antialiased">
 
-
-        {/* Google Analytics */}
         <Script
-          src="https://www.googletagmanager.com/gtag/js?id=G-5FLYTCH3C7"
-          strategy="afterInteractive"
-        />
+  src="https://www.googletagmanager.com/gtag/js?id=G-5FLYTCH3C7"
+  strategy="afterInteractive"
+/>
 
-        <Script id="google-analytics" strategy="afterInteractive">
-          {`
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
-
-            gtag('config', 'G-5FLYTCH3C7');
-          `}
-        </Script>
-
+<Script id="google-analytics" strategy="afterInteractive">
+{`
+window.dataLayer = window.dataLayer || [];
+function gtag(){window.dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-5FLYTCH3C7');
+`}
+</Script>
 
         {/* Google SEO Schema */}
         <script
